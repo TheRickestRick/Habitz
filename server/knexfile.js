f80f9c3e5967bf9f86092c0d9dd17b38a338c3a3
Ryan Wittrup <ryan.wittrup@gmail.com>
2018-01-10 15:55:41 -0600

    updates knex with production settings for RDS instance

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
 
   development: {
@@ -16,7 +18,13 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.DB_HOST,
+      port: 5432,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: "habitz_production"
+    }
   }
 
 };
