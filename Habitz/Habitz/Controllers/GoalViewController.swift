499567366202d8f308a466215feb4cf507b46350
Ryan Wittrup <ryan.wittrup@gmail.com>
2018-01-13 12:43:39 -0600

    adds picker to goal view controller to restrict values for percentToComplete

--- Habitz/Habitz/Controllers/GoalViewController.swift
+++ Habitz/Habitz/Controllers/GoalViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class GoalViewController: UIViewController, UITextFieldDelegate {
+class GoalViewController: UIViewController, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
     
     // MARK: - Properties
     @IBOutlet weak var saveButton: UIBarButtonItem!
@@ -20,6 +20,9 @@ class GoalViewController: UIViewController, UITextFieldDelegate {
     // passed in by GoalTableViewController or constructed as part of adding a new goal
     var goal: Goal?
     
+    // data for picker to restrict values
+    let pickerData = [0, 25, 50, 75, 100]
+    let percentToCompletePicker: UIPickerView! = UIPickerView()
     
     
     override func viewDidLoad() {
@@ -28,12 +31,21 @@ class GoalViewController: UIViewController, UITextFieldDelegate {
         // handle the text field's input through delegate callbacks
         nameTextField.delegate = self
         
+        // handle selecting percentToComplete through delegate callbacks
+        percentToCompletePicker.dataSource = self
+        percentToCompletePicker.delegate = self
+        
+        
+        percentToBeCompleteTextField.inputView = percentToCompletePicker
+        
         // sets up views if editing an existing goal
         if let goal = goal {
             navigationItem.title = goal.name
             nameTextField.text = goal.name
             percentToBeCompleteTextField.text = String(goal.percentToBeComplete)
             completedStreakTextField.text = String(goal.completedStreak)
+        } else {
+            percentToBeCompleteTextField.text = String(pickerData[0])
         }
         
         // enable save button only if the text field has a valid goal name
@@ -88,6 +100,25 @@ class GoalViewController: UIViewController, UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) -> Void {
         updateSaveButtonState()
         navigationItem.title = textField.text
+    }
+    
+    
+    //MARK: - UIPickerViewDelegate
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(pickerData[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        percentToBeCompleteTextField.text = String(pickerData[row])
+        self.view.endEditing(true)
     }
     
     
