45b98dc178b7be7cb52cfcb36181ac60ec4f8c2d
Ryan Wittrup <ryan.wittrup@gmail.com>
2018-01-14 17:24:14 -0600

    adds completed_streak field to habits table

---
+++ server/migrations/20180114172159_habits3.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('habits', table => {
+    table.integer("completed_streak").defaultTo(0);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('goals', table => {
+    table.dropColumn("completed_streak");
+  });
+};
--- server/seeds/3_habits.js
+++ server/seeds/3_habits.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('habits').insert([
-        {id: 1, name: 'Lift weights', goal_id: 1},
-        {id: 2, name: 'Meditate', goal_id: 1},
-        {id: 3, name: 'Call an old friend', goal_id: 2},
-        {id: 4, name: 'Go to a networking event', goal_id: 3},
-        {id: 5, name: 'Complete one job application', goal_id: 3}
+        {id: 1, name: 'Lift weights', goal_id: 1, completed_streak: 4},
+        {id: 2, name: 'Meditate', goal_id: 1, completed_streak: 1},
+        {id: 3, name: 'Call an old friend', goal_id: 2, completed_streak: 0},
+        {id: 4, name: 'Go to a networking event', goal_id: 3, completed_streak: 14},
+        {id: 5, name: 'Complete one job application', goal_id: 3, completed_streak: 3}
       ]);
     });
 };
