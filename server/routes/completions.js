52e04b9406d258b7b561f765ac1083ea369e250a
Ryan Wittrup <ryan.wittrup@gmail.com>
2018-01-15 14:15:15 -0600

    updates completion route to get by user id and current date

--- server/routes/completions.js
+++ server/routes/completions.js
@@ -3,25 +3,32 @@ const router = express.Router()
 const knex = require('../db')
 
 router.get('/', (req, res, next) => {
-  // TODO - query string to search by user_uid
-  // if (req.query.user_uid !== undefined) {
-  //   knex('completions')
-  //     .where({user_uid: req.query.user_uid})
-  //     .then(goals => res.json(goals))
-  //     .catch(err => next(err))
-  //   return;
-  // }
-
-  // return only completions for the current date
-  if (req.query.today === 'true') {
-    console.log('get all completions for current date');
-    knex('completions')
-      .orderBy('id', 'asc')
-      .where(knex.raw('created_at >= CURRENT_DATE'))
-      .then(completions => res.json(completions))
+  // search by user_uid, returning only completions for the current day
+  if (req.query.user_uid !== undefined) {
+    knex.from('completions')
+      .join('habits', 'habits.id', 'completions.habit_id')
+      .join('goals', 'goals.id', 'habits.goal_id')
+      .select('habits.id', 'habits.name', 'habits.goal_id', 'habits.completed_streak', 'completions.created_at')
+      .where({'goals.user_uid': req.query.user_uid})
+      .andWhere(knex.raw('completions.created_at >= CURRENT_DATE'))
+      .orderBy('habits.id', 'asc')
+      .then(habits => {
+        res.json(habits)
+      })
       .catch(err => next(err))
     return;
   }
+
+  // return only completions for the current date
+  // if (req.query.today === 'true') {
+  //   console.log('get all completions for current date');
+  //   knex('completions')
+  //     .orderBy('id', 'asc')
+  //     .where(knex.raw('created_at >= CURRENT_DATE'))
+  //     .then(completions => res.json(completions))
+  //     .catch(err => next(err))
+  //   return;
+  // }
 
   knex('completions')
     .orderBy('id', 'asc')
